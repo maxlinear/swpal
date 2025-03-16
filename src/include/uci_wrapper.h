456055e13ca9ab4ad2259ff9f122cca652b2fd24
kesavareddy <kesavareddy@maxlinear.com>
2025-03-16 11:07:03 -0700

    URX PRPLOS Public Release UPDK_9.1.80

--- src/include/uci_wrapper.h
+++ src/include/uci_wrapper.h
@@ -1,3 +1,15 @@
+/******************************************************************************
+
+         Copyright (c) 2020 - 2025, MaxLinear, Inc.
+         Copyright 2016 - 2020 Intel Corporation
+         Copyright 2015 - 2016 Lantiq Beteiligungs-GmbH & Co. KG
+         Copyright 2009 - 2014 Lantiq Deutschland GmbH
+         Copyright 2007 - 2008 Infineon Technologies AG
+
+  For licensing information, see the file 'LICENSE' in the root folder of
+  this software module.
+
+*******************************************************************************/
 #ifndef __UCI_WRAPPER_API_H_
 #define __UCI_WRAPPER_API_H_
 
